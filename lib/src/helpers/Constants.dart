009de91aa67b062e7ea06b4195aa51e36e989a85
lchoua <loikchoua4@gmail.com>
2020-11-29 22:04:41 -0300

    correcion URL

--- lib/src/helpers/Constants.dart
+++ lib/src/helpers/Constants.dart
@@ -1,4 +1,4 @@
 abstract class Constants{
-  static const String URL = "http://127.0.0.1:1323";
-  //static const String URL = "https://zmgestion.uc.r.appspot.com";
+  //static const String URL = "http://127.0.0.1:1323";
+  static const String URL = "https://zmgestion.uc.r.appspot.com";
 }
